39c99e3ae84a9b7c0edb451a9459c47895ea3e26
Jen Glaubius <glaubius@gmail.com>
2025-02-14 10:12:37 -0500

    added preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://jeg-test-astro.netlify.app/"
+  site: "https://jeg-test-astro.netlify.app/",
+  integrations: [preact()]
 });
